72c6701829e57785705aeabdafc85a0964cea33a
ZenithSUS <jeranmerino147@gmail.com>
2025-03-20 14:34:15 +0800

    refactor createInfo and getInfo functions for improved error handling and return types

--- src/appwrite/users.ts
+++ src/appwrite/users.ts
@@ -1,27 +1,25 @@
 import { databases, ID, DATABASE_ID, COLLECTION_ID } from ".";
 import { Users, AddUser } from "../libs/types";
 
-export async function createInfo(data: AddUser): Promise<Object | void> {
+export async function createInfo(data: AddUser) {
   try {
-    const response = await databases.createDocument(
+    return await databases.createDocument(
       DATABASE_ID,
       COLLECTION_ID,
       ID.unique(),
       data
     );
-    console.log(response);
-    return response;
   } catch (error) {
     console.error(error);
   }
 }
 
-export default async function getInfo(): Promise<Users[] | void> {
+export default async function getInfo(): Promise<Users[]> {
   try {
     const { documents } = await databases.listDocuments(DATABASE_ID, COLLECTION_ID);
     console.log(documents);
     return documents as Users[];
   } catch (error) {
-    console.error(error);
+    throw new Error(error as string);
   }
 }
